1207f425337168dc23b7d9b49b39d0901c673b89
jcarloshg <carlosj12336@gmail.com>
2025-10-02 18:26:17 -0600

    add test

--- test/src/app/shared/infrastructure/criteria/criteria-cursor-to-sql.test.ts
+++ test/src/app/shared/infrastructure/criteria/criteria-cursor-to-sql.test.ts
@@ -1,33 +1,40 @@
 import { CriteriaCursor } from "@/app/shared/domain/repository/criteria-cursor/criteria-cursor.criteria-cursor";
-import { FilterCursor } from "@/app/shared/domain/repository/criteria-cursor/filter-cursor.criteria-cursor";
 import { OrderCursor, OrderCursorType } from "@/app/shared/domain/repository/criteria-cursor/order-cursor.criteria-cursor";
 import { PaginationCursor } from "@/app/shared/domain/repository/criteria-cursor/pagination-cursor.criteria-cursor";
 import { CriteriaCursorToSql } from "@/app/shared/infrastructure/criteria/criteria-cursor-to-sql";
 
-describe('CriteriaCursorToSql', () => {
-    it('should convert criteria to SQL query', () => { });
+
+describe("criteria-cursor-to-sql.test", () => {
+    it("should convert criteria to SQL query", () => { });
 
     // const filterCursor = new FilterCursor();
     const paginationCursor = new PaginationCursor();
     const orderCursor = new OrderCursor("price", OrderCursorType.ASC);
-    const criteriaCursor = new CriteriaCursor(
-        [],
-        paginationCursor,
-        orderCursor
-    );
+    const criteriaCursor = new CriteriaCursor([], paginationCursor, orderCursor);
 
-    const selectBody = '*';
-    const fromBody = 'product';
+    const selectBody = "*";
+    const fromBody = "product";
 
-    const propertiesMap: Map<string, string> = new Map([
-        ['id', '1']
+    const propertiesMap: Map<string, string> = new Map<string, string>([
+        ["uuid", "product.uuid"],
+        ["name", "product.name"],
+        ["description", "product.description"],
+        ["availability", "product.availability"],
+        ["price", "product.price"],
+        ["rating", "product.rating"],
+        ["brandName", "brand.name"],
+        ["categoryName", "category.name"],
+        ["attributeValue", "product_attribute.value"],
     ]);
 
-    const criteria = new CriteriaCursorToSql(criteriaCursor, selectBody, fromBody);
+    const criteria = new CriteriaCursorToSql(
+        criteriaCursor,
+        selectBody,
+        fromBody
+    );
     const querySql = criteria.toSql(propertiesMap);
 
     console.log(`querySql: `, querySql);
 
-    expect(querySql).toBe('SELECT * FROM product WHERE id = 1');
-
+    expect(querySql).toBe("SELECT * FROM product WHERE id = 1");
 });
