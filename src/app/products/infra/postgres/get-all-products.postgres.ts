ee6499c895a7a3a960d03af473faa59b5a6a1611
jcarloshg <carlosj12336@gmail.com>
2025-10-05 19:45:30 -0600

    remove unicesary comments

--- src/app/products/infra/postgres/get-all-products.postgres.ts
+++ src/app/products/infra/postgres/get-all-products.postgres.ts
@@ -45,60 +45,6 @@ export class GetAllProductsPostgres implements GetAllProductsRepository {
             ["attributeValue", "product_attribute.value"],
         ]);
 
-        // const criteriaParser = new CriteriaToSql();
-        // const parameterizedWhere = criteriaParser.getParameterizedWhereClause(propertiesMap);
-        // const order = criteriaParser.getOrderByClause(propertiesMap);
-        // const pagination = criteriaParser.getPaginationClause(propertiesMap);
-
-        // const baseQuery = `
-        // SELECT
-        //     product.uuid as uuid,
-        //     product.name as name,
-        //     product.description as description,
-        //     product.availability as availability,
-        //     product.price as price,
-        //     product.rating as rating,
-        //     array_agg(
-        //         jsonb_build_object(
-        //             'uuid',
-        //             attribute.uuid,
-        //             'value',
-        //             product_attribute.value,
-        //             'name',
-        //             attribute.name
-        //         )
-        //     ) AS attributes,
-        //     jsonb_build_object(
-        //         'uuid',
-        //         brand.uuid,
-        //         'name',
-        //         brand.name
-        //     ) AS brand,
-        //     jsonb_build_object(
-        //         'uuid',
-        //         category.uuid,
-        //         'name',
-        //         category.name
-        //     ) AS category
-        // FROM
-        //     product
-        //     JOIN product_attribute ON product.uuid = product_attribute.product_id
-        //     JOIN attribute ON product_attribute.attribute_id = attribute.uuid
-        //     JOIN brand ON product.brand_id = brand.uuid
-        //     JOIN category ON product.category_id = category.uuid
-
-        // ${parameterizedWhere.query}
-
-        // GROUP BY
-        //     product.uuid,
-        //     brand.uuid,
-        //     category.uuid
-
-        // ${order}
-
-        // ${pagination}
-        // ;`;
-
         const criteriaToSql = new CriteriaToSql()
             .buildSelect([
                 "product.uuid as uuid",
